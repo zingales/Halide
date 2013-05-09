8fb1d9dd3ce0b0bbaab23f3c8b71e7dbc92711ce
Nick Chornay <nickchornay@gmail.com>
2013-05-09 12:25:07 -0400

    full set of storage reordering tests

--- cpp/apps/blur_diagonal/test.cpp
+++ cpp/apps/blur_diagonal/test.cpp
@@ -25,53 +25,53 @@ typedef CImg<uint16_t> Image;
 
 extern "C" {
 #include "halide_blur.h"
-#include "halide_blur_cvl.h"
+#include "halide_blur_tiled.h"
 }
 
 // Convert a CIMG image to a buffer_t for halide
 buffer_t halideBufferOfImage(Image &im) {
     buffer_t buf = {(uint8_t *)im.data(), 0, false, false, 
-                    {im.width(), im.height(), 3, 1}, 
-                    {1, im.width(), im.width()*im.height(), 0}, 
+                    {im.width(), im.height(), 1, 1}, 
+                    {1, im.width(), 0, 0}, 
                     {0, 0, 0, 0}, 
                     sizeof(int16_t)};
     return buf;
 }
 
 Image blur_halide(Image &in) {
-    Image out(in.width(), in.height(), 3);
+    Image out(in.width(), in.height());
 
-    buffer_t inbuf = halideBufferOfImage(in);
+    //buffer_t inbuf = halideBufferOfImage(in);
     buffer_t outbuf = halideBufferOfImage(out);
 
     // Call it once to initialize the halide runtime stuff
-    halide_blur(&inbuf, &outbuf);
+    halide_blur(&outbuf);
 
     begin_timing;
     
     // Compute the same region of the output as blur_fast (i.e., we're
     // still being sloppy with boundary conditions)
-    halide_blur(&inbuf, &outbuf);
+    halide_blur(&outbuf);
 
     end_timing;
 
     return out;
 }
 
-Image blur_halide_cvl(Image &in) {
-    Image out(in.width(), in.height(), 3);
+Image blur_halide_tiled(Image &in) {
+    Image out(in.width(), in.height());
 
-    buffer_t inbuf = halideBufferOfImage(in);
+    //buffer_t inbuf = halideBufferOfImage(in);
     buffer_t outbuf = halideBufferOfImage(out);
 
     // Call it once to initialize the halide runtime stuff
-    halide_blur_cvl(&inbuf, &outbuf);
+    halide_blur_tiled(&outbuf);
 
     begin_timing;
     
     // Compute the same region of the output as blur_fast (i.e., we're
     // still being sloppy with boundary conditions)
-    halide_blur_cvl(&inbuf, &outbuf);
+    halide_blur_tiled(&outbuf);
 
     end_timing;
 
@@ -80,31 +80,35 @@ Image blur_halide_cvl(Image &in) {
 
 int main(int argc, char **argv) {
 
-    Image input(6400, 4800, 3);
+    Image input(2048, 2048);
 
-    for (int c = 0; c < 3; c++) {
-	for (int y = 0; y < input.height(); y++) {
-	    for (int x = 0; x < input.width(); x++) {
-		input(x, y, c) = rand() & 0xfff;
-	    }
+    for (int y = 0; y < input.height(); y++) {
+	for (int x = 0; x < input.width(); x++) {
+	    input(x, y) = rand() & 0xfff;
 	}
     }
 
     Image halide = blur_halide(input);
     float halide_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
 
-    Image halide_cvl = blur_halide_cvl(input);
-    float halide_cvl_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
+    printf("not dead yet\n");
+
+    Image halide_tiled = blur_halide_tiled(input);
+    float halide_tiled_time = (t2.tv_sec - t1.tv_sec) + (t2.tv_usec - t1.tv_usec) / 1000000.0f;
     
-    printf("times (halide, halide_cvl): %f %f\n", halide_time, halide_cvl_time);
-    for (int c = 0; c < 3; c++) {
-	for (int y = 0; y < input.height(); y++) {
-	    for (int x = 0; x < input.width(); x++) {
-		if (halide(x, y, c) != halide_cvl(x, y, c))
-		    printf("difference at (%d,%d,%d): %d %d\n", x, y, c,
-			   halide(x, y, c), halide_cvl(x, y, c));
-	    }
+    printf("still not dead yet\n");
+
+    printf("times (halide, halide_tiled): %f %f\n", halide_time, halide_tiled_time);
+
+    printf("still still not dead yet\n");
+
+    /*for (int y = 0; y < input.height(); y++) {
+	for (int x = 0; x < input.width(); x++) {
+	    if (halide(x, y) != halide_tiled(x, y))
+		printf("difference at (%d,%d): %d %d\n", x, y,
+		       halide(x, y), halide_tiled(x, y));
 	}
-    }
+	}*/
+    
     return 0;
 }
