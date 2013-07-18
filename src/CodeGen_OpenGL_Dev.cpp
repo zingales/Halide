6433298d18c4bfe1468ddea47d3683a97f181fcb
Nick Chornay <nickchornay@gmail.com>
2013-07-17 23:48:43 -0400

    some progress towards getting loads to work in openGL codegen

--- src/CodeGen_OpenGL_Dev.cpp
+++ src/CodeGen_OpenGL_Dev.cpp
@@ -19,27 +19,38 @@ CodeGen_OpenGL_Dev::CodeGen_OpenGL_Dev() {
 
 string CodeGen_OpenGL_Dev::CodeGen_OpenGL_C::print_type(Type type) {
     ostringstream oss;
-    assert(type.width == 1 && "Can't codegen vector types to OpenGL C (yet)");
-    if (type.is_float()) {
-        // TODO figure out how many bits openGL floats have
-        if (type.bits == 32) {
-            oss << "float";
+    if (type.is_scalar()) {
+        if (type.is_float()) {
+            if (type.bits == 32) {
+                oss << "float";
+            } else {
+                assert(false && "Can't represent a float with this many bits in OpenGL ES SL");
+            }
         } else {
-            assert(false && "Can't represent a float with this many bits in OpenGL C");
+            if (type.bits == 1) {
+                oss << "bool";
+            } else {
+                assert(type.is_int() && "Can't represent an unsigned integer in OpenGL ES SL");
+                if (type.bits == 32) {
+                    oss << "int";
+                } else {
+                    assert("Can't represent an integer with this many bits in OpenGL ES SL");
+                }
+            }
         }
+    } else if (type.width <= 4) {
+        if (type.is_bool()) {
+            oss << "b";
+        } else if (type.is_int()) {
+            oss << "i";
+        } else if (!type.is_float()) {
+            assert(type.is_int() && "Can't represent an unsigned integer in OpenGL ES SL, let alone a vector of them");
+        }
+        oss << "vec" << type.width;
+        // TODO width
     } else {
-        // TODO can't represent unsigned ints in glsl?
-        switch (type.bits) {
-        case 1:
-            oss << "bool";
-            break;
-        case 32:
-            oss << "int";
-            break;
-        default:
-            assert(false && "Can't represent an integer with this many bits in OpenGL C");
-        }
-    }
+        assert(false && "Can't codegen vector types wider than 4 in OpenGL ES SL");
+    }        
     return oss.str();
 }
 
@@ -67,30 +78,33 @@ void CodeGen_OpenGL_Dev::CodeGen_OpenGL_C::visit(const For *loop) {
 }
 
 void CodeGen_OpenGL_Dev::CodeGen_OpenGL_C::visit(const Load *op) {
-    //stream << "#if 0\n";
-    //CodeGen_C::visit(op);
-    //stream << "#endif\n";
-    do_indent();
-    stream << "texture("
-           << print_name(op->name)
-           << ", "
-           << print_expr(op->index)
-           << ");";
+    ostringstream rhs;
+    //Type f1 = Float(32, 1);
+    string idx_1d = print_expr(op->index);
+    string x = "pixcoord.x"; //print_assignment(f1, "mod(" + idx_1d + ", dim_of_" + op->name + ".x);");
+    string y = "pixcoord.y"; //print_assignment(f1, idx_1d + "/dim_of_" + op->name + ".y);");
+    string idx_2d = "vec2(" + x + "," + y + ")";
+    rhs << "texture2D("
+        << print_name(op->name)
+        << ", "
+        << idx_2d
+        << ").x";
+    //Type f4 = Float(32, 4);
+    print_assignment(op->type, rhs.str());
 }
 
 void CodeGen_OpenGL_Dev::CodeGen_OpenGL_C::visit(const Store *op) {
-    //stream << "#if 0\n";
-    //CodeGen_C::visit(op);
-    //stream << "#endif\n";
     do_indent();
     stream << OUTPUT_NAME_DELIMITER
            << op->name
            << OUTPUT_NAME_DELIMITER
            << "\n";
+    string val = print_expr(op->value);
     do_indent();
+    // TODO: handle higher dimensional outputs (this only works in 2D)
     stream << "gl_FragColor = "
            << "vec4("
-           << print_expr(op->value)
+           << val
            << ",0,0,1)"
            << ";\n";
 }
@@ -117,10 +131,13 @@ void CodeGen_OpenGL_Dev::CodeGen_OpenGL_C::compile(Stmt s, string name, const ve
             // add dimensions argument
             stream << "uniform sampler2D "
                    << print_name(args[i].name)
-                   << ";\n"
+                   << "; //"
+                   << print_type(args[i].type)
+                   << "\n"
                    << "uniform ivec4 dim_of_"
                    << print_name(args[i].name)
                    << ";\n";
+
         } else {
             stream << "uniform "
                << print_type(args[i].type)
--- test/hello_gl.cpp
+++ test/hello_gl.cpp
@@ -11,7 +11,7 @@ int main(int argc, char **argv) {
     printf("Defining function...\n");
 
     g(x, y) = x + y;
-    f(x, y) = 2; //g(x, y)*g(x,y);
+    f(x, y) = g(x+1,y)*g(x+1,y);
 
     char *target = getenv("HL_TARGET");
     g.compute_root();
@@ -26,7 +26,7 @@ int main(int argc, char **argv) {
     // Check the result was what we expected
     for (int i = 0; i < 32; i++) {
         for (int j = 0; j < 32; j++) {
-            if (imf(i, j) != 2) { //(i + j)*(i + j)) {
+            if (imf(i, j) != (i + j)*(i + j)) {
                 printf("imf[%d, %d] = %d\n", i, j, imf(i, j));
                 return -1;
             }
